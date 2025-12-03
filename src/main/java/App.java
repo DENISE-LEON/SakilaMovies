de11d74f2dbbe7bab673725e45da51e87f5da845
deniseleon <kbenisee7@gmail.com>
2025-12-02 20:15:53 -0500

    sakila pt 1

---
+++ src/main/java/App.java
@@ -0,0 +1,138 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class App {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //password username check
+        if (args.length != 2) {
+            System.out.println("Application needs two args to run: A username and a password for the db");
+            //exit the app due to failure because we dont have a username and password from the command line
+            System.exit(1);
+        }
+        System.out.println("password and username found");
+
+        //username and password from args
+        String username = args[0];
+        String password = args[1];
+
+        String url = "jdbc:mysql://localhost:3306/sakila";
+
+        //new data source object
+        BasicDataSource dataSource = new BasicDataSource();
+
+        //setting the url, password, username
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        try (Connection connection = dataSource.getConnection()) {
+            menuOptions(connection);
+        } catch (SQLException e) {
+            System.out.println("An error has occurred" + " " + e.getMessage());
+        }
+    }
+
+    public static void menuOptions(Connection connection) {
+        boolean run = true;
+        while (run) {
+            System.out.println("What would you like to do");
+            System.out.println("""
+                    1) Last name actor search
+                    2) Full name actor search
+                    0) Exit
+                    """);
+            int menuChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (menuChoice) {
+                case 1:
+                    lastNameSearch(connection);
+                    break;
+                case 2:
+                    fullNameSearch(connection);
+                    break;
+                case 0:
+                    run = false;
+                    break;
+            }
+        }
+    }
+
+    public static void lastNameSearch(Connection connection) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("""
+                SELECT first_name,
+                last_name
+                FROM actor
+                WHERE last_name = ?;
+                """)) {
+            System.out.println("Enter the last name of your favorite actor:");
+            String lastName = scanner.nextLine().toUpperCase().trim();
+            preparedStatement.setString(1, lastName);
+
+            try (ResultSet set = preparedStatement.executeQuery()
+            ) {
+                printResults(set);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+
+    public static void fullNameSearch(Connection connection) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("""
+                SELECT first_name,
+                last_name
+                FROM actor
+                WHERE first_name = ? AND last_name = ?;
+                """)) {
+            System.out.println("Enter the first name:");
+            String firstName = scanner.nextLine().toUpperCase().trim();
+
+            System.out.println("Enter the last name:");
+            String lastName = scanner.nextLine().toUpperCase().trim();
+
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+
+            try (ResultSet set = preparedStatement.executeQuery()
+            ) {
+                printResults(set);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error" + " " + e.getMessage());
+        }
+    }
+
+    public static void printResults(ResultSet resultSet) {
+        try {
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            //bool for handling no matches
+            boolean any = true;
+
+            //result set starts before first row, therefore need next
+            while (resultSet.next()) {
+                any = true;
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+
+                    String value = resultSet.getString(i);
+                    System.out.printf("%-15s: %-20s%n", columnName, value);
+                }
+
+                //print an empty line to make the results prettier
+                System.out.println("--------------------------------");
+            }
+            if (!any) {
+                System.out.println("No matches found");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error" + " " + e.getMessage());
+        }
+    }
+}
