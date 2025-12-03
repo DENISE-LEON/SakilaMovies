8229fd2ab8e7279a9b681b4376594788e1725de5
deniseleon <kbenisee7@gmail.com>
2025-12-03 13:40:21 -0500

    create DataManager, film, and actor classes

---
+++ src/main/java/Film.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Film {
+    private int filmID;
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private int length;
+}
